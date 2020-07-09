02ab72a9b5dca3edb9e6c1ccd1472a264f214914
jdepalma <33504952+jdepalma@users.noreply.github.com>
2020-07-09 16:48:51 -0400

    Create Export_Container_Assessments.ps1

---
+++ Export_Container_Assessments.ps1
@@ -0,0 +1,43 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$Subscription,
+	[Parameter(Mandatory = $true)]
+    [string]$FileName
+	)
+
+
+
+Function Export-Container-Assessments($subscription ,$filename) {
+
+$context = Get-AzContext
+$profile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+$profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($profile)
+$token = $profileClient.AcquireAccessToken($context.Subscription.TenantId)
+$authHeader = @{
+	'Content-Type'  = 'application/json' 
+	'Authorization' = 'Bearer ' + $token.AccessToken 
+    }
+	
+	
+$url = "https://management.azure.com/subscriptions/$($subscription)/providers/Microsoft.Security/subAssessments?api-version=2019-01-01-preview"
+
+$results = (Invoke-RestMethod -Method "Get" -Uri $url -Headers $authHeader ).value
+
+foreach ($result in $results){
+$id = $result.properties.id
+$severity = $result.properties.status.severity
+$remediation = $result.properties.remediation
+$displayName = $result.properties.displayName
+$impact = $result.properties.impact
+$category = $result.properties.category
+$imageDigest = $result.properties.additionalData.imageDigest
+$repositoryName = $result.properties.additionalData.repositoryName
+$type = $result.properties.additionalData.type
+[pscustomobject]@{ id = $id; severity =  $severity; remediation = $remediation; displayName = $displayName; impact = $impact; category = $category; imageDigest = $imageDigest; repositoryName = $repositoryName; type = $type } | Export-Csv $filename -Append -NoTypeInformation
+}
+echo "Done"
+}
+
+Export-Container-Assessments $Subscription $FileName
+
